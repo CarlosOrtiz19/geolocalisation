e96038cea207e5e614bd602fa5ea60269117535e
carlosOrtiz19 <carlos.arturo.ortiz@hotmail.com>
2020-04-05 22:32:08 -0400

    Ajoutez des fichiers projet.

---
+++ GeoShare/GeoShare/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace GeoShare
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private string latitude;
+        private string longitude;
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+    
+
+        private async void Get_Location(object sender, EventArgs e)
+        {
+
+            
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Latitude.Text = "Latitude: "  + location.Latitude.ToString();
+                    Longitude.Text = "Longitude: " + location.Longitude.ToString();
+                    Altitude.Text = "Altitude: " + location.Altitude.ToString();
+
+                    latitude = location.Latitude.ToString();
+                    longitude = location.Longitude.ToString();
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+
+        }
+
+        private void Share_Location(object sender, EventArgs e)
+        {
+            SendSms(latitude + " " + longitude, Telephone.Text);
+        }
+
+        public async Task SendSms(string messageText, string recipient)
+        {
+            try
+            {
+                var message = new SmsMessage(messageText, new[] { recipient });
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
